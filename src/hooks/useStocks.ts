7c715c486c60697f86cfb5645b087cb6dabc3be4
JungMin <wjdals0304@gmail.com>
2025-04-03 12:22:51 +0900

    feat: 리뷰 반영

--- src/hooks/useStocks.ts
+++ src/hooks/useStocks.ts
@@ -6,9 +6,6 @@ export const useStocks = (tab: StockTabType, period: StockPeriodType) => {
     queryKey: ['stocks', tab, period],
     queryFn: async () => {
       const response = await fetch(`/api/stocks?tab=${tab}&period=${period}`);
-      if (!response.ok) {
-        throw new Error('주식 데이터를 불러오는데 실패했습니다.');
-      }
       return response.json();
     },
   });
--- src/pages/api/stocks/index.ts
+++ src/pages/api/stocks/index.ts
@@ -9,32 +9,44 @@ import {
   STOCK_PERIOD,
   type StockTabType,
 } from '@/constants/stockConstants';
+import { StockPeriodType } from '@/constants/stockConstants';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     const { tab, period = STOCK_PERIOD.REALTIME } = req.query;
 
-    const getStocksByPeriod = (type: StockTabType) => {
-      if (period === STOCK_PERIOD.WEEK) {
-        return type === STOCK_TAB.VOLUME
-          ? weeklyVolumeStocks
-          : weeklyVolumeCountStocks;
-      }
-      return type === STOCK_TAB.VOLUME
-        ? realtimeVolumeStocks
-        : realtimeVolumeCountStocks;
-    };
-
     switch (tab) {
       case STOCK_TAB.VOLUME:
-        return res.status(200).json(getStocksByPeriod(STOCK_TAB.VOLUME));
+        return res
+          .status(200)
+          .json(getStocksByPeriod(STOCK_TAB.VOLUME, period as StockPeriodType));
       case STOCK_TAB.VOLUME_COUNT:
-        return res.status(200).json(getStocksByPeriod(STOCK_TAB.VOLUME_COUNT));
+        return res
+          .status(200)
+          .json(
+            getStocksByPeriod(
+              STOCK_TAB.VOLUME_COUNT,
+              period as StockPeriodType,
+            ),
+          );
       default:
-        return res.status(200).json(getStocksByPeriod(STOCK_TAB.VOLUME));
+        return res
+          .status(200)
+          .json(getStocksByPeriod(STOCK_TAB.VOLUME, period as StockPeriodType));
     }
   } else {
     res.setHeader('Allow', ['GET']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
+
+const getStocksByPeriod = (type: StockTabType, period: StockPeriodType) => {
+  if (period === STOCK_PERIOD.WEEK) {
+    return type === STOCK_TAB.VOLUME
+      ? weeklyVolumeStocks
+      : weeklyVolumeCountStocks;
+  }
+  return type === STOCK_TAB.VOLUME
+    ? realtimeVolumeStocks
+    : realtimeVolumeCountStocks;
+};
