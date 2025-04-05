3ecd4aa193178f7509675962e184733796df82dd
JungMin <wjdals0304@gmail.com>
2025-04-05 14:56:04 +0900

    feat: 뉴스 데이터 연동

--- src/pages/NewsSection/NewsSection.tsx
+++ src/pages/NewsSection/NewsSection.tsx
@@ -1,60 +1,25 @@
-import Image from 'next/image';
-
-const NewsItemList = [
-  {
-    title: '소호은행 소상공인 생애주기 맞춤형 서비스 제공',
-    time: '54분전 딜사이트',
-    image:
-      'https://static.tossinvestcdn.com/assets/image/detail-news-default/image7.png',
-  },
-  {
-    title: '소호은행 소상공인 생애주기 맞춤형 서비스 제공',
-    time: '54분전 딜사이트',
-    image:
-      'https://static.tossinvestcdn.com/assets/image/detail-news-default/image7.png',
-  },
-];
+import { useNewsQuery } from '@/hooks/useNewsQuery';
+import NewsItems from './NewsItems';
 
 export default function NewsSection() {
+  const { data, isError } = useNewsQuery();
+
+  if (isError) {
+    return (
+      <div className="min-h-[340px] flex items-center justify-center">
+        <span className="text-red-500">뉴스를 불러오는데 실패했습니다.</span>
+      </div>
+    );
+  }
   return (
     <div className="min-h-[340px]">
       <div className="flex gap-4 mb-2">
-        <button>
-          <span className="text-[15px] font-normal text-lightGray">
-            주요 뉴스
-          </span>
-        </button>
-        <button>
-          <span className="text-[15px] font-normal text-lightGray">
-            추천 뉴스
-          </span>
-        </button>
+        <span className="text-[15px] font-normal text-lightGray">
+          주요 뉴스
+        </span>
       </div>
       <div className="flex flex-col gap-4">
-        {NewsItemList.map(item => (
-          <div
-            key={item.title}
-            className="flex justify-between items-center p-[8px 0]"
-          >
-            <div className="flex flex-col">
-              <span className="text-[15px] font-normal text-lightGray">
-                {item.title}
-              </span>
-              <span className="text-[13px] font-normal text-darkGray">
-                {item.time}
-              </span>
-            </div>
-            <div className="w-[64px] h-[64px] border-[10px] ml-[8px]">
-              <Image
-                src={item.image}
-                alt="news1"
-                width={64}
-                height={64}
-                className="w-[100%] h-[100%] object-cover rounded-[10px]"
-              />
-            </div>
-          </div>
-        ))}
+        {data?.items.map(item => <NewsItems key={item.id} item={item} />)}
       </div>
     </div>
   );
