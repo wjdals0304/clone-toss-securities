08f5e2f811b6ee837874d2bbc74f8fe316e874fe
JungMin <wjdals0304@gmail.com>
2025-04-05 11:20:04 +0900

    feat: 지수 연동

--- src/pages/Index/type.ts
+++ src/pages/Index/type.ts
@@ -1,5 +1,43 @@
-interface IndexItem {
+export enum IndexSectionType {
+  INDEX = '지수',
+  BOND = '채권',
+  MATERIAL = '원자재',
+}
+
+export enum DataType {
+  INDEX = 'index',
+  BOND = 'bond',
+  MATERIAL = 'material',
+}
+
+interface BaseItem {
+  id: number;
   name: string;
+  type: DataType;
+}
+
+export interface IndexItemType extends BaseItem {
+  type: DataType.INDEX;
+  price: number;
+  change: number;
+  changeRate: number;
+}
+
+export interface BondItemType {
+  period: string;
+  price: number;
+  change: number;
+  changeRate: number;
+  type: DataType.BOND;
+}
+
+export interface BondItemListType extends BaseItem {
+  type: DataType.BOND;
+  items: BondItemType[];
+}
+
+export interface MaterialItemType extends BaseItem {
+  type: DataType.MATERIAL;
   price: number;
   change: number;
   changeRate: number;
